3066e711d8db04af1392fd45ccd96f42b6298a06
rubberdk <dnu04242@gmail.com>
2021-05-05 15:35:51 -0500

    whisk over one object

--- code/scripts/run_env_active.sh
+++ code/scripts/run_env_active.sh
@@ -10,9 +10,8 @@
 --OBJECT 4 \
 --ACTIVE 1 \
 --TIME_STOP 1.0 \
---WHISKER_NAMES R \
---ORIENTATION 0.17 0 1.6 \
---POSITION -95 45 -140 \
+--ORIENTATION 0 0 0 \
+--POSITION 0 0 0 \
 --SAVE_VIDEO 0 \
 --SAVE 0 \
 --file_video "../output/video_env5_rock.mp4" \
