b23a076bc6f1a4a8f1dc89f682d0df076f041b77
rubberdk <dnu04242@gmail.com>
2021-06-21 14:32:24 -0500

    cylinders 1 to 5 added

--- code/scripts/run_custom_env.sh
+++ code/scripts/run_custom_env.sh
@@ -7,16 +7,16 @@
 --BLOW 1 \
 --DEBUG 0 \
 --OBJECT 5 \
---file_env "../data/environment/scan_97.obj" \
+--file_env "../data/cylinders/c1.obj" \
 --ACTIVE 1 \
 --TIME_STOP 1.0 \
 --SAVE_VIDEO 1 \
 --SAVE 1 \
---ObjX 30 \
---ObjY 0 \
---ObjZ 5 \
---ObjYAW 1.57 \
---ObjPITCH -0.7 \
+--ObjX 40 \
+--ObjY 20 \
+--ObjZ 0 \
+--ObjYAW 0 \
+--ObjPITCH 0 \
 --ObjROLL 0 \
 --file_video "../output/video_object.mp4" \
 --dir_out ../output/full_array_env_active
