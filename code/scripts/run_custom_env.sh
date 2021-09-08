06db725a906fffb1953800bdada6a002e1c3cb2d
dokkev <dhk6869@gmail.com>
2021-09-08 10:18:53 -0500

    now it runs cylinder1

--- code/scripts/run_custom_env.sh
+++ code/scripts/run_custom_env.sh
@@ -1,6 +1,6 @@
 
 ../build/whiskit_gui \
---PRINT 2 \
+--PRINT 1 \
 --CONNECT 0 \
 --CDIST 50 \
 --CPITCH -0 \
@@ -13,12 +13,12 @@
 --SAVE_VIDEO 1 \
 --SAVE 1 \
 --OBJECT 5 \
---file_env "../data/environment/scan_97.obj" \
---ObjX 30 \
---ObjY 0 \
---ObjZ 5 \
---ObjYAW 1.57 \
---ObjPITCH -0.7 \
+--file_env "../data/cylinders/c1.obj" \
+--ObjX 45 \
+--ObjY 10 \
+--ObjZ 0 \
+--ObjYAW 0 \
+--ObjPITCH 0 \
 --ObjROLL 0 \
 --file_video "../output/video_object.mp4" \
 --dir_out ../output/full_array_env_active
