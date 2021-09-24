aa5ab6f4bab9ab8cd1fca906c85f3f660ded3154
rubberdk <dnu04242@gmail.com>
2021-09-24 15:06:12 -0500

    concave questsim setup

--- code/include/Simulation.h
+++ code/include/Simulation.h
@@ -29,8 +29,8 @@ Copyright (c) 2015 Google Inc. http://bulletphysics.org
 #include "Simulation_utility.h"
 #include "Simulation_IO.h"
 
-#include "Interface.hpp"
-#include "State.hpp"
+// #include "Interface.hpp"
+// #include "State.hpp"
 
 #include <iostream>
 #include <chrono> 
