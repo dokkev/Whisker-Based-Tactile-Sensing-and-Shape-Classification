f909d8c9cf652a3cff8e299c9e7b323acc55346d
dokkev <dhk6869@gmail.com>
2021-10-11 16:52:18 -0500

    data to image

--- code/trainer/classifier_example.py
+++ code/trainer/classifier_example.py
@@ -1,31 +1,80 @@
-import numpy as np
-import pandas as pd
-import seaborn as sns
-import matplotlib.pyplot as plt
-
 import torch
 import torch.nn as nn
-import torch.optim as optim
-from torch.utils.data import Dataset, DataLoader
-
-from sklearn.preprocessing import StandardScaler    
+import numpy as np
+from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import confusion_matrix, classification_report
+import pandas as pd
 
-
-# read data using pandas
-df = pd.read_csv("Dataset_spine.csv")
-
-df.head()
+# 0) Prepare data
+# print(bc)
+df = pd.read_csv('../results/data_s/F_total.csv', header = None)
 # print(df)
-sns.countplot(x = 'Class_att', data=df)
+print("data loaded")
+X = df.loc[:, 1:2] #features vectors
+y = df.loc[:, 3]   #class labels: 2 = benign, 4 = malignant
+X = np.array(X)
+y = np.array(y)
 
-df['Class_att'] = df['Class_att'].astype('category')
-encode_map = {
-    'Abnormal': 1,
-    'Normal': 0
-}
+# X, y = bc.data, bc.target
 
-df['Class_att'].replace(encode_map, inplace=True)
+n_samples, n_features = X.shape
 
-print(df)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, random_state=1)
+
+# scale
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+
+X_train = torch.from_numpy(X_train.astype(np.float32))
+X_test = torch.from_numpy(X_test.astype(np.float32))
+y_train = torch.from_numpy(y_train.astype(np.float32))
+y_test = torch.from_numpy(y_test.astype(np.float32))
+
+y_train = y_train.view(y_train.shape[0], 1)
+y_test = y_test.view(y_test.shape[0], 1)
+
+# 1) Model
+# Linear model f = wx + b , sigmoid at the end
+class Model(nn.Module):
+    def __init__(self, n_input_features):
+        super(Model, self).__init__()
+        self.linear = nn.Linear(n_input_features, 1)
+
+    def forward(self, x):
+        y_pred = torch.sigmoid(self.linear(x))
+        return y_pred
+
+model = Model(n_features)
+
+# 2) Loss and optimizer
+num_epochs = 500
+learning_rate = 0.01
+criterion = nn.BCELoss()
+optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
+
+# 3) Training loop
+for epoch in range(num_epochs):
+    # Forward pass and loss
+    y_pred = model(X_train)
+    loss = criterion(y_pred, y_train)
+
+    # Backward pass and update
+    loss.backward()
+    optimizer.step()
+
+    # zero grad before new step
+    optimizer.zero_grad()
+
+    if (epoch+1) % 10 == 0:
+        print(f'epoch: {epoch+1}, loss = {loss.item():.4f}')
+PATH = 'Model.pth'
+torch.save(model.state_dict(), PATH)
+print("saved")
+
+
+with torch.no_grad():
+    y_predicted = model(X_test)
+    y_predicted_cls = y_predicted.round()
+    acc = y_predicted_cls.eq(y_test).sum() / float(y_test.shape[0])
+    print(f'accuracy: {acc.item():.4f}')
